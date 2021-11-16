429c0e34929cdabe8bdf826667b996666f576796
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2021-11-16 23:22:56 +0800

    generate Device

--- Orka/Device.cs
+++ Orka/Device.cs
@@ -34,7 +34,7 @@ public class FullDevice : ShortDevice
 
 public class DeviceVersion
 {
-    public string Incremental { get; set; }
+    public uint Incremental { get; set; }
     public string Release { get; set; }
     public string CodeName { get; set; }
     public int Sdk { get; set; }
